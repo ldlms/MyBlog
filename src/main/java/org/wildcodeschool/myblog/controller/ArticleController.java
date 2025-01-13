e311f4144a285b7200104061b3a712469e53f883
Leo <leodelmas31@gmail.com>
2025-01-13 12:21:21 +0100

    CRUD

---
+++ src/main/java/org/wildcodeschool/myblog/controller/ArticleController.java
@@ -0,0 +1,77 @@
+package org.wildcodeschool.myblog.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.wildcodeschool.myblog.model.Article;
+import org.wildcodeschool.myblog.repository.ArticleRepository;
+
+@RestController
+@RequestMapping("/articles")
+public class ArticleController {
+
+	private final ArticleRepository articleRepository;
+	
+	public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+	
+	public ResponseEntity<List<Article>> getAllArticle(){
+		List<Article> articles = articleRepository.findAll();
+		if(articles.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(articles);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Article> getArticleById(@PathVariable Long id){
+		Article article = articleRepository.findById(id).orElse(null);
+		if(article == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(article);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Article> createArticle(@RequestBody Article article){
+		article.setCreatedAt(LocalDateTime.now());
+		article.setUpdatedAt(LocalDateTime.now());
+		Article savedArticle = articleRepository.save(article);
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Article> updateArticle(@PathVariable Long id,@RequestBody Article articleDetails){
+		Article article = articleRepository.findById(id).orElse(null);
+		if(article == null) {
+			return ResponseEntity.notFound().build();
+		}
+		article.setTitle(articleDetails.getTitle());
+		article.setContent(articleDetails.getContent());
+		article.setUpdatedAt(LocalDateTime.now());
+		
+		Article updatedArticle = articleRepository.save(article);
+		return ResponseEntity.ok(updatedArticle);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Article> deleteArticle(@PathVariable Long id){
+		Article article = articleRepository.findById(id).orElse(null);
+		if(article == null) {
+			return ResponseEntity.notFound().build();
+		} 
+		articleRepository.delete(article);
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ src/main/java/org/wildcodeschool/myblog/repository/ArticleRepository.java
@@ -0,0 +1,8 @@
+package org.wildcodeschool.myblog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.wildcodeschool.myblog.model.Article;
+
+public interface ArticleRepository extends JpaRepository<Article,Long> {
+
+}
